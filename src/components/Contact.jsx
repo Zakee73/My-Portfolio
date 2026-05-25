fde4286a7343cef371439e3738c447face43f6ba
Mohd Zakee <mohdzakee73@gmail.com>
2026-05-25 17:40:16 +0530

    Implement form submission in Contact component
    Added form submission logic to the Contact component.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,31 @@
 import React from 'react'
 
 const Contact = ({darkMode}) => {
+   const [result, setResult] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "0c95425b-d07d-4b5f-a0b1-6bda9dae0d2b");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+        } else {
+            console.log("Error", data);
+            setResult(data.message);
+        }
+    };
+  
   return (
    <section 
    id='contact'
